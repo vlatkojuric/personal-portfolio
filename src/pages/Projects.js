9ab7228029448f9f704634cebc428f95f5388649
vlatkojuric <juricv78@gmail.com>
2024-01-19 01:03:08 +0100

    initial commit

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,16 +4,43 @@ import MovieApp from "../assets/movie-search-app.png";
 import PhotoApp from "../assets/photo-search-app.png";
 import TravelApp from "../assets/travel-list-app.png";
 import "../styles/Projects.css";
+import { Link } from "react-router-dom";
 
 export default function Projects() {
   return (
     <div className="projects">
       <h1>My Projects</h1>
       <div className="projectList">
-        <ProjectItem name="Travel List App" image={TravelApp} />
-        <ProjectItem name="Movie Search App" image={MovieApp} />
-        <ProjectItem name="Photo Search app" image={PhotoApp} />
-        <ProjectItem name="Crypto App" image={CryptoApp} />
+        <Link
+          to="https://travelingoo.netlify.app/"
+          target="_blank"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <ProjectItem name="Travel List App" image={TravelApp} />
+        </Link>
+        <Link
+          to="https://filmoplex.netlify.app/"
+          target="_blank"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <ProjectItem name="Movie Search App" image={MovieApp} />
+        </Link>
+
+        <Link
+          to="https://photofindapp.netlify.app/"
+          target="_blank"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <ProjectItem name="Photo Search app" image={PhotoApp} />
+        </Link>
+
+        <Link
+          to="https://cryptopricesapp.netlify.app/"
+          target="_blank"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <ProjectItem name="Crypto App" image={CryptoApp} />
+        </Link>
       </div>
     </div>
   );

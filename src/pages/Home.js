b873b3253fb97d28c26e4c8765b43646f2d946bf
vlatkojuric <juricv78@gmail.com>
2024-02-24 18:58:45 +0100

    initial commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,20 +8,20 @@ export default function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2>Hi, I'm Vlatko,Your Next Software Engineer</h2>
+        <h2>Hi, I'm Vlatko,Your Next Web Developer</h2>
         <div className="prompt">
           <p>
             I love answers! <br />
             <br />
             Previously an Neue Fische- School and Pool for Digital Talent
             Trainee and I am always thinking about code and this passion often
-            leads me to solve my coding problem, while I’m jogging, watching
+            leads me to solve my coding problem, while I'm jogging, watching
             documentaries, or cooking fajitas for breakfast. I'll figure out the
             answer then rush to my computer to see it work.
             <br />
-            <br /> My name is Vlatko Juric. I’m a passionate software developer
-            who will do whatever it takes to find an answer. Get in touch to
-            find the next answer together!
+            <br /> My name is Vlatko Juric. I'm a passionate web developer who
+            will do whatever it takes to find an answer. Get in touch to find
+            the next answer together!
           </p>
           <Link to="https://www.linkedin.com/in/vlatkojuric/" target="_blank">
             <LinkedInIcon />
